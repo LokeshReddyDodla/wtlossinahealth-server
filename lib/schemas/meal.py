855d4a5df7ca82155b4b87b0fba137ccb5f0328a
Mukhtar Ahmed <mukhtar@ahealth.in>
2024-07-16 14:15:17 +0530

    meal schema created

---
+++ lib/schemas/meal.py
@@ -0,0 +1,40 @@
+from typing import Optional
+
+from pydantic import BaseModel
+from typing import List, Optional
+
+
+class NutritionalValues(BaseModel):
+    calories: str
+    proteins: str
+    carbohydrates: str
+    fats: str
+    fiber: str
+
+
+class FoodItem(BaseModel):
+    name: str
+    coordinates: Optional[List[float]]
+    serving_size: str
+    serving_quantity: str
+    serving_unit: str
+    nutritional_values: NutritionalValues
+
+
+class TotalNutritionalValue(BaseModel):
+    calories: str
+    proteins: str
+    carbohydrates: str
+    fats: str
+    fiber: str
+
+
+class FoodDescription(BaseModel):
+    meal_type: str
+    items: List[FoodItem]
+    total_nutritional_value: TotalNutritionalValue
+    image_url: str
+    description: Optional[str] = None
+    feedback: str
+    tags: List[str]
+    context_id: str
