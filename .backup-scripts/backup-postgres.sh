317ac4098a049ae1ded653ddf3fd03a025098ce7
Mukhtar Ahmed <mukhtar@ahealth.in>
2024-08-04 02:33:04 +0530

    -

--- .backup-scripts/backup-postgres.sh
+++ .backup-scripts/backup-postgres.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Source the .env file
+export $(grep -v '^#' /home/deployer/aihealth-deployment/aihealth-server/.env | xargs)
+
 # Set the date format for the backup file
 DATE=$(date +"%Y%m%d%H%M")
 
--- .backup-scripts/backup.sh
+++ .backup-scripts/backup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Source environment variables if needed
-if [ -f ../.env ]; then
-    source ../.env
+if [ -f /home/deployer/aihealth-deployment/aihealth-server/.env ]; then
+    source /home/deployer/aihealth-deployment/aihealth-server/.env
 fi
 
 # Run all backup scripts
 ./backup-redis.sh
-# ./backup-postgres.sh
-# ./backup-mongo.sh
-# ./backup-clickhouse.sh
+./backup-postgres.sh
+./backup-mongo.sh
+./backup-clickhouse.sh
 
 echo "All backups completed."
