f0931ab0e75135c3c51f24bf6b72756eb1645c16
Mukhtar Ahmed <mukhtar@ahealth.in>
2024-08-04 02:56:03 +0530

    backup-mongo.sh updated

--- .backup-scripts/backup-mongo.sh
+++ .backup-scripts/backup-mongo.sh
@@ -17,12 +17,26 @@ mkdir -p "$TEMP_DIR"
 # Docker container name
 CONTAINER_NAME="aihealth-mongo"
 
-# Create backup
-docker exec $CONTAINER_NAME mongodump --archive=$TEMP_DIR/mongo_backup_$DATE.gz --gzip
+# MongoDB credentials
+MONGO_USERNAME=$MONGO_INITDB_ROOT_USERNAME
+MONGO_PASSWORD=$MONGO_INITDB_ROOT_PASSWORD
+MONGO_AUTH_DB=$MONGOEXPRESS_LOGIN
+
+# Create backup inside the container
+docker exec $CONTAINER_NAME mongodump --archive="/data/db/mongo_backup_$DATE.gz" --gzip --username=$MONGO_USERNAME --password=$MONGO_PASSWORD --authenticationDatabase=$MONGO_AUTH_DB
 
 # Check if the backup was successful
 if [ $? -ne 0 ]; then
   echo "MongoDB backup failed."
+  exit 1
+fi
+
+# Copy the backup file from the container to the host
+docker cp $CONTAINER_NAME:/data/db/mongo_backup_$DATE.gz "$TEMP_DIR/mongo_backup_$DATE.gz"
+
+# Check if the copy was successful
+if [ $? -ne 0 ]; then
+  echo "Failed to copy the MongoDB backup file from the container."
   exit 1
 fi
 
