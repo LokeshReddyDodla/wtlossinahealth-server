f682035ebfa326c77a566dfce3c22711902093a1
Mukhtar Ahmed <mukhtar@ahealth.in>
2024-08-08 12:06:23 +0530

    patient_connected_app schema, model created

---
+++ lib/schemas/patient_connected_app.py
@@ -0,0 +1,51 @@
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+from uuid import UUID
+
+
+class PatientConnectedAppBase(BaseModel):
+    patient_id: UUID
+
+
+class PatientConnectedApp(PatientConnectedAppBase):
+    id: UUID
+
+    class Config:
+        orm_mode = True
+
+
+class LibreViewBase(BaseModel):
+    connected_app_id: UUID
+    libreview_id: str
+    last_sync_timestamp: Optional[datetime] = None
+    connected_at: Optional[datetime] = None
+
+
+class LibreViewCreate(LibreViewBase):
+    pass
+
+
+class LibreView(LibreViewBase):
+    id: UUID
+
+    class Config:
+        orm_mode = True
+
+
+class OtherAppBase(BaseModel):
+    connected_app_id: UUID
+    other_app_id: str
+    additional_field: Optional[str] = None
+    connected_at: Optional[datetime] = None
+
+
+class OtherAppCreate(OtherAppBase):
+    pass
+
+
+class OtherApp(OtherAppBase):
+    id: UUID
+
+    class Config:
+        orm_mode = True
