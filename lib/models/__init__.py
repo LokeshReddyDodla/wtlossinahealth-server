e5b98d12b41fe40e765199173c922fefc0ded751
Mukhtar Ahmed <mukhtar@ahealth.in>
2024-08-13 03:20:21 +0530

    Update __init__.py

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -10,3 +10,4 @@ from lib.models.patient_vitals import *
 from lib.models.patient_smbg import *
 from lib.models.meal import *
 from lib.models.admin import *
+from lib.models.fitness_data_sync import *
