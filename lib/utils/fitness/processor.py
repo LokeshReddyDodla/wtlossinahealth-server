a9e7f9f283d692684cdbc9b31b4c394eb042dae9
Mukhtar Ahmed <mukhtar@ahealth.in>
2024-08-17 12:04:21 +0530

    FitnessDataProcessor code refac

--- lib/utils/fitness/processor.py
+++ lib/utils/fitness/processor.py
@@ -1,5 +1,4 @@
 from datetime import datetime, timedelta
-import math
 from typing import List, Dict, Optional
 from lib.schemas.fitness import (
     FitnessActivityDistribution,
@@ -7,7 +6,6 @@ from lib.schemas.fitness import (
     FitnessPeakActivityTime,
     FitnessDailyStats,
     FitnessSummaryStats,
-    FitnessWeekOverWeekComparison,
     FitnessWeeklyStats,
     FitnessMonthlyStats,
     FitnessHourlyStats,
@@ -23,7 +21,6 @@ from lib.utils.fitness.queries import (
     generate_monthly_stats_query,
     generate_hourly_stats_query,
 )
-from pandas import DataFrame
 
 
 class FitnessDataProcessor:
@@ -38,9 +35,107 @@ class FitnessDataProcessor:
             self.patient_id, from_date_str, to_date_str
         )
         result = self.clickhouse_store.client.execute(query)
-        total_steps = result[0][0]
-        total_active_energy = result[0][1]
-        total_active_duration = result[0][2]
+        return self._construct_fitness_summary_stats(
+            result[0],
+            from_date_str,
+            to_date_str,
+            FitnessSummaryStats,
+            index_starts=0,
+        )
+
+    def fetch_daily_stats(
+        self, from_date_str: str, to_date_str: str
+    ) -> List[FitnessDailyStats]:
+        query = generate_daily_stats_query(
+            self.patient_id, from_date_str, to_date_str
+        )
+        data = self.clickhouse_store.client.execute(query)
+
+        daily_stats = []
+        for row in data:
+            date = row[0]
+            stats_instance = self._construct_fitness_summary_stats(
+                result=row,
+                from_date_str=date.strftime("%Y-%m-%dT00:00:00"),
+                to_date_str=date.strftime("%Y-%m-%dT23:59:59"),
+                stats_class=FitnessDailyStats,
+                additional_fields={"date": date},
+            )
+            daily_stats.append(stats_instance)
+        return daily_stats
+
+    def fetch_weekly_stats(
+        self, from_date_str: str, to_date_str: str
+    ) -> List[FitnessWeeklyStats]:
+        query = generate_weekly_stats_query(
+            self.patient_id, from_date_str, to_date_str
+        )
+        data = self.clickhouse_store.client.execute(query)
+
+        weekly_stats = []
+        for row in data:
+            week_number = row[0]
+            stats_instance = self._construct_fitness_summary_stats(
+                result=row,
+                from_date_str=from_date_str,
+                to_date_str=to_date_str,
+                stats_class=FitnessWeeklyStats,
+                additional_fields={"week_number": week_number},
+            )
+            weekly_stats.append(stats_instance)
+        return weekly_stats
+
+    def fetch_monthly_stats(
+        self, from_date_str: str, to_date_str: str
+    ) -> List[FitnessMonthlyStats]:
+        query = generate_monthly_stats_query(
+            self.patient_id, from_date_str, to_date_str
+        )
+        data = self.clickhouse_store.client.execute(query)
+
+        monthly_stats = []
+        for row in data:
+            month = row[0]
+            stats_instance = self._construct_fitness_summary_stats(
+                result=row,
+                from_date_str=from_date_str,
+                to_date_str=to_date_str,
+                stats_class=FitnessMonthlyStats,
+                additional_fields={"month": month},
+            )
+            monthly_stats.append(stats_instance)
+        return monthly_stats
+
+    def fetch_hourly_stats(
+        self, from_date_str: str, to_date_str: str
+    ) -> List[FitnessHourlyStats]:
+        query = generate_hourly_stats_query(
+            self.patient_id, from_date_str, to_date_str
+        )
+        data = self.clickhouse_store.client.execute(query)
+        hourly_stats = [
+            FitnessHourlyStats(
+                hour=row[0],
+                steps=row[1],
+                active_energy=row[2],
+                active_duration=row[3],
+            )
+            for row in data
+        ]
+        return hourly_stats
+
+    def _construct_fitness_summary_stats(
+        self,
+        result,
+        from_date_str: str,
+        to_date_str: str,
+        stats_class,
+        index_starts: int = 1,
+        additional_fields: Dict = {},
+    ):
+        steps = result[index_starts]
+        active_energy = result[index_starts + 1]
+        active_duration = result[index_starts + 2]
 
         # Calculate average active session duration
         avg_active_session_query = (
@@ -51,7 +146,6 @@ class FitnessDataProcessor:
         avg_active_session_result = self.clickhouse_store.client.execute(
             avg_active_session_query
         )
-
         average_active_session_duration = (
             avg_active_session_result[0][0] if avg_active_session_result else 0
         )
@@ -76,237 +170,16 @@ class FitnessDataProcessor:
         )
         inactive_periods = self._fetch_inactive_periods(inactive_periods_query)
 
-        return FitnessSummaryStats(
-            steps=total_steps,
-            active_energy=total_active_energy,
-            active_duration=total_active_duration,
+        return stats_class(
+            steps=steps,
+            active_energy=active_energy,
+            active_duration=active_duration,
             average_active_session_duration=average_active_session_duration,
             activity_distribution=activity_distribution,
             peak_activity_time=peak_activity_time,
             inactive_periods=inactive_periods,
+            **additional_fields
         )
-
-    def fetch_daily_stats(
-        self, from_date_str: str, to_date_str: str
-    ) -> List[FitnessDailyStats]:
-        query = generate_daily_stats_query(
-            self.patient_id, from_date_str, to_date_str
-        )
-        data = self.clickhouse_store.client.execute(query)
-
-        daily_stats = []
-        for row in data:
-            date = row[0]
-            steps = row[1]
-            active_energy = row[2]
-            active_duration = row[3]
-
-            # Calculate average active session duration
-            avg_active_session_query = (
-                generate_average_active_session_duration_query(
-                    self.patient_id,
-                    date.strftime("%Y-%m-%dT00:00:00"),
-                    date.strftime("%Y-%m-%dT23:59:59"),
-                )
-            )
-            avg_active_session_result = self.clickhouse_store.client.execute(
-                avg_active_session_query
-            )
-            average_active_session_duration = (
-                avg_active_session_result[0][0]
-                if avg_active_session_result
-                else 0
-            )
-
-            activity_distribution_query = generate_activity_distribution_query(
-                self.patient_id,
-                date.strftime("%Y-%m-%dT00:00:00"),
-                date.strftime("%Y-%m-%dT23:59:59"),
-            )
-            activity_distribution = self._fetch_activity_distribution(
-                activity_distribution_query
-            )
-
-            peak_activity_time_query = generate_peak_activity_time_query(
-                self.patient_id,
-                date.strftime("%Y-%m-%dT00:00:00"),
-                date.strftime("%Y-%m-%dT23:59:59"),
-            )
-            peak_activity_time = self._fetch_peak_activity_time(
-                peak_activity_time_query
-            )
-
-            inactive_periods_query = generate_inactive_periods_query(
-                self.patient_id,
-                date.strftime("%Y-%m-%dT00:00:00"),
-                date.strftime("%Y-%m-%dT23:59:59"),
-            )
-            inactive_periods = self._fetch_inactive_periods(
-                inactive_periods_query
-            )
-
-            daily_stats.append(
-                FitnessDailyStats(
-                    date=date,
-                    steps=steps,
-                    active_energy=active_energy,
-                    active_duration=active_duration,
-                    average_active_session_duration=average_active_session_duration,
-                    activity_distribution=activity_distribution,
-                    peak_activity_time=peak_activity_time,
-                    inactive_periods=inactive_periods,
-                )
-            )
-
-        return daily_stats
-
-    def fetch_weekly_stats(
-        self, from_date_str: str, to_date_str: str
-    ) -> List[FitnessWeeklyStats]:
-        query = generate_weekly_stats_query(
-            self.patient_id, from_date_str, to_date_str
-        )
-        data = self.clickhouse_store.client.execute(query)
-        weekly_stats = []
-        for row in data:
-            week_number = row[0]
-            steps = row[1]
-            active_energy = row[2]
-            active_duration = row[3]
-
-            # Calculate average active session duration
-            avg_active_session_query = (
-                generate_average_active_session_duration_query(
-                    self.patient_id, from_date_str, to_date_str
-                )
-            )
-            avg_active_session_result = self.clickhouse_store.client.execute(
-                avg_active_session_query
-            )
-            average_active_session_duration = (
-                avg_active_session_result[0][0]
-                if avg_active_session_result
-                else 0
-            )
-
-            activity_distribution_query = generate_activity_distribution_query(
-                self.patient_id, from_date_str, to_date_str
-            )
-            activity_distribution = self._fetch_activity_distribution(
-                activity_distribution_query
-            )
-
-            peak_activity_time_query = generate_peak_activity_time_query(
-                self.patient_id, from_date_str, to_date_str
-            )
-            peak_activity_time = self._fetch_peak_activity_time(
-                peak_activity_time_query
-            )
-
-            inactive_periods_query = generate_inactive_periods_query(
-                self.patient_id, from_date_str, to_date_str
-            )
-            inactive_periods = self._fetch_inactive_periods(
-                inactive_periods_query
-            )
-
-            weekly_stats.append(
-                FitnessWeeklyStats(
-                    week_number=week_number,
-                    steps=steps,
-                    active_energy=active_energy,
-                    active_duration=active_duration,
-                    average_active_session_duration=average_active_session_duration,
-                    activity_distribution=activity_distribution,
-                    peak_activity_time=peak_activity_time,
-                    inactive_periods=inactive_periods,
-                )
-            )
-
-        return weekly_stats
-
-    def fetch_monthly_stats(
-        self, from_date_str: str, to_date_str: str
-    ) -> List[FitnessMonthlyStats]:
-        query = generate_monthly_stats_query(
-            self.patient_id, from_date_str, to_date_str
-        )
-        data = self.clickhouse_store.client.execute(query)
-        monthly_stats = []
-        for row in data:
-            month = row[0]
-            steps = row[1]
-            active_energy = row[2]
-            active_duration = row[3]
-
-            # Calculate average active session duration
-            avg_active_session_query = (
-                generate_average_active_session_duration_query(
-                    self.patient_id, from_date_str, to_date_str
-                )
-            )
-            avg_active_session_result = self.clickhouse_store.client.execute(
-                avg_active_session_query
-            )
-            average_active_session_duration = (
-                avg_active_session_result[0][0]
-                if avg_active_session_result
-                else 0
-            )
-
-            activity_distribution_query = generate_activity_distribution_query(
-                self.patient_id, from_date_str, to_date_str
-            )
-            activity_distribution = self._fetch_activity_distribution(
-                activity_distribution_query
-            )
-
-            peak_activity_time_query = generate_peak_activity_time_query(
-                self.patient_id, from_date_str, to_date_str
-            )
-            peak_activity_time = self._fetch_peak_activity_time(
-                peak_activity_time_query
-            )
-
-            inactive_periods_query = generate_inactive_periods_query(
-                self.patient_id, from_date_str, to_date_str
-            )
-            inactive_periods = self._fetch_inactive_periods(
-                inactive_periods_query
-            )
-
-            monthly_stats.append(
-                FitnessMonthlyStats(
-                    month=month,
-                    steps=steps,
-                    active_energy=active_energy,
-                    active_duration=active_duration,
-                    average_active_session_duration=average_active_session_duration,
-                    activity_distribution=activity_distribution,
-                    peak_activity_time=peak_activity_time,
-                    inactive_periods=inactive_periods,
-                )
-            )
-
-        return monthly_stats
-
-    def fetch_hourly_stats(
-        self, from_date_str: str, to_date_str: str
-    ) -> List[FitnessHourlyStats]:
-        query = generate_hourly_stats_query(
-            self.patient_id, from_date_str, to_date_str
-        )
-        data = self.clickhouse_store.client.execute(query)
-        hourly_stats = [
-            FitnessHourlyStats(
-                hour=row[0],
-                steps=row[1],
-                active_energy=row[2],
-                active_duration=row[3],
-            )
-            for row in data
-        ]
-        return hourly_stats
 
     def _fetch_activity_distribution(
         self, query: str
