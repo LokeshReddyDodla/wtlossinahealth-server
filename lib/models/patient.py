01e51d4bbdb8e91aa364a2d385e6e279e2044277
Mukhtar Ahmed <mukhtar@ahealth.in>
2024-08-22 16:58:16 +0530

    patient_token_usage_log created

--- lib/models/patient.py
+++ lib/models/patient.py
@@ -155,6 +155,10 @@ class Patient(Base):
         "PatientSleep", back_populates="patient", cascade="all, delete-orphan"
     )
 
+    token_usage_logs = relationship(
+        "TokenUsageLog", back_populates="patient", cascade="all, delete-orphan"
+    )
+
 
 @listens_for(Patient, "after_insert")
 def create_related_records(mapper, connection, target):
