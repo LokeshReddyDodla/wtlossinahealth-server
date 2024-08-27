d0bdb18163ab24c5df118e75b577b2535a43e78d
Mukhtar Ahmed <mukhtar@ahealth.in>
2024-08-27 16:01:44 +0530

    /verify-otp response updated

--- rest_server/auth/api_schema.py
+++ rest_server/auth/api_schema.py
@@ -1,9 +1,16 @@
 from pydantic import BaseModel
 from rest_server.response_models import SuccessResponse
 
+
 class OTPRequest(BaseModel):
     phone_number: str
+
 
 class OTPVerifyRequest(BaseModel):
     phone_number: str
     otp: str
+
+
+class OtpVerifyResponse(BaseModel):
+    token: str
+    is_new_user: bool
--- rest_server/auth/auth.py
+++ rest_server/auth/auth.py
@@ -4,6 +4,10 @@ from fastapi import APIRouter, HTTPException, Request
 from lib.core.otp import create_and_send_otp, verify_otp
 from lib.utils.auth_utils import AuthUtils
 from lib.utils.jwt import create_jwt_token
+from rest_server.auth.api_schema import (
+    OtpVerifyResponse,
+    OtpVerifySuccessResponse,
+)
 from rest_server.response_models import ErrorResponse, SuccessResponse
 from sqlalchemy.future import select
 
@@ -21,22 +25,31 @@ async def generate_otp(request: Request, user_phone: UserPhoneNumber):
         return ErrorResponse(message="Failed to generate OTP", detail=str(e))
 
 
-@router.post("/verify-otp", tags=["Auth"], response_model=SuccessResponse)
+@router.post(
+    "/verify-otp",
+    tags=["Auth"],
+    response_model=OtpVerifySuccessResponse,
+)
 async def verify_otp_endpoint(request: Request, user_otp: UserOTP, role: str):
     try:
         cache_store = request.state.context.otp_store
         if await verify_otp(user_otp.phone_number, user_otp.otp, cache_store):
             async with request.state.context.postgres_store.get_session() as session:
                 auth_utils = AuthUtils(session)
-                user, user_id = await auth_utils.get_or_create_user(
-                    user_otp.phone_number, role
+                user, user_id, is_new_user = (
+                    await auth_utils.get_or_create_user(
+                        user_otp.phone_number, role
+                    )
                 )
                 token = create_jwt_token(
                     user_id=user_id,
                     role=role,
                 )
-                return SuccessResponse(
-                    message="OTP verified", data={"token": token}
+                return OtpVerifySuccessResponse(
+                    message="OTP verified",
+                    data=OtpVerifyResponse(
+                        token=token, is_new_user=is_new_user
+                    ),
                 )
         else:
             raise HTTPException(status_code=400, detail="Invalid OTP")
