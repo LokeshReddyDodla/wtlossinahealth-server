3004746d482ec07f4fc879f5419f54a46623870d
Mukhtar Ahmed <mukhtar@ahealth.in>
2024-08-28 13:45:50 +0530

    meal analysis response updated

--- rest_server/prescriptions/analyze_prescription.py
+++ rest_server/prescriptions/analyze_prescription.py
@@ -10,7 +10,7 @@ from lib.core.auth_bearer import handler
 from lib.utils.json_parsing import parse_json_garbage
 from lib.utils.openai.meal_analysis import get_nutritional_info
 from lib.utils.openai.prescription_analysis import analyze_prescription
-from rest_server.meals.api_schema import MealDescription, MealAnalysisResponse
+from rest_server.meals.api_schema import MealDescription
 from rest_server.prescriptions.api_schema import (
     Medicine,
     PrescriptionAnalysisResponse,
