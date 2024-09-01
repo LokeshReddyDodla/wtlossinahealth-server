4ee6c3d87866383ad0cecd8daa2b9da0cc34504d
Mukhtar Ahmed <mukhtar@ahealth.in>
2024-09-02 02:30:41 +0530

    Update auth.py

--- rest_server/auth/auth.py
+++ rest_server/auth/auth.py
@@ -52,11 +52,10 @@ async def verify_otp_endpoint(request: Request, user_otp: UserOTP, role: str):
                     ),
                 )
         else:
-            response = ErrorResponse(
+            return ErrorResponse(
                 message="Invalid OTP",
                 detail="The OTP provided is incorrect.",
             )
-            raise HTTPException(status_code=400, detail=response.dict())
     except HTTPException as e:
         raise e
     except Exception as e:
