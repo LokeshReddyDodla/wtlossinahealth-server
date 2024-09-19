23ceaea68b307809a94af3cb90c575acf6f12fcb
Mukhtar Ahmed <mukhtar@ahealth.in>
2024-09-19 17:04:56 +0530

    Update api_schema.py

--- rest_server/chats/api_schema.py
+++ rest_server/chats/api_schema.py
@@ -2,3 +2,8 @@ from datetime import datetime
 from typing import List, Literal, Optional, Union
 
 from pydantic import BaseModel, Field, HttpUrl, constr
+
+from lib.schemas.chat import ChatSchema
+from rest_server.response_models import SuccessResponse
+
+ChatResponse = SuccessResponse[ChatSchema]
