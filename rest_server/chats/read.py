bec253225ad0f7372d309b270b533a62195eef9c
Mukhtar Ahmed <mukhtar@ahealth.in>
2024-09-19 16:58:59 +0530

    Update read.py

--- rest_server/chats/read.py
+++ rest_server/chats/read.py
@@ -1,12 +1,14 @@
 from fastapi import Depends, HTTPException, Request
 
 from lib.dependencies.auth.base import get_current_user
+from lib.schemas.chat import ChatSchema
 from lib.services.chat_service import ChatService
+from rest_server.chats.api_schema import ChatResponse
 
 from .router import router
 
 
-@router.get("")
+@router.get("", response_model=ChatResponse)
 async def get_user_chats(
     request: Request, current_user=Depends(get_current_user)
 ):
@@ -15,7 +17,11 @@ async def get_user_chats(
     try:
         async with request.state.context.postgres_store.get_session() as session:
             chats = await chat_service.get_user_chats(user_id, session)
-            return chats
+
+            return ChatResponse(
+                message="Chats fetched successfully",
+                data=ChatSchema.from_orm(chats),
+            )
 
     except HTTPException as e:
         raise e
