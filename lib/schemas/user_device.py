77175d01113ee688a88b75997d8b9fc984e69437
Mukhtar Ahmed <mukhtar@ahealth.in>
2024-09-28 19:56:05 +0530

    -

--- lib/schemas/user_device.py
+++ lib/schemas/user_device.py
@@ -4,17 +4,23 @@ from uuid import UUID
 
 from pydantic import BaseModel
 
+from lib.core.types import ProfileTypeLiteral
+
 
 class UserDeviceBase(BaseModel):
     user_id: UUID
-    profile_type: str  # 'patient' or 'care_provider'
+    profile_type: ProfileTypeLiteral
     fcm_token: str
     device_type: Optional[str] = None  # e.g., 'iOS', 'Android'
     platform_version: Optional[str] = None
     last_updated_at: Optional[datetime] = None
+    patient_id: Optional[UUID] = None
+    care_provider_id: Optional[UUID] = None
+
 
 class UserDeviceCreate(UserDeviceBase):
     pass
+
 
 class UserDevice(UserDeviceBase):
     device_id: UUID
