b22a81444bddde3b126c566a225d9ffd822c8ed6
Mukhtar Ahmed <mukhtar@ahealth.in>
2024-10-18 13:08:46 +0530

    -

--- rest_server/patients/overview/api_schema.py
+++ rest_server/patients/overview/api_schema.py
@@ -2,7 +2,7 @@ from typing import Any, Optional
 
 from pydantic import BaseModel
 
-from lib.schemas.glucose_stats import GlucoseOverallReport
+from lib.schemas.glucose_stats import GlucoseLevelStats
 from lib.schemas.meal_stats import DailyMealStats
 from rest_server.response_models import SuccessResponse
 
@@ -10,7 +10,7 @@ from rest_server.response_models import SuccessResponse
 class PatientOverview(BaseModel):
     meal_stats: Optional[DailyMealStats] = None
     fitness_stats: Optional[Any] = None
-    glucose_stats: Optional[GlucoseOverallReport] = None
+    glucose_stats: Optional[GlucoseLevelStats] = None
 
 
 PatientOverviewResponse = SuccessResponse[PatientOverview]
