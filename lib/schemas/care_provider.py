ad96c4674d59c935bae41b49fb44ea577a1972ce
Mukhtar Ahmed <mukhtar@ahealth.in>
2024-11-01 01:34:23 +0530

    bug fix

--- lib/schemas/care_provider.py
+++ lib/schemas/care_provider.py
@@ -17,7 +17,8 @@ class CareProviderBase(BaseModel):
     permissions: Optional[Dict[str, Dict[str, bool]]] = None
     created_at: Optional[datetime]
     updated_at: Optional[datetime]
-    is_verified: bool
+    is_verified: Optional[bool] = False
+
 
 
 
