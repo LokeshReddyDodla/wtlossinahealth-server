7bd5cc9070fe44e51a3a0a436deb19c83d420beb
Mukhtar Ahmed <mukhtar@ahealth.in>
2024-11-04 23:32:42 +0530

    set smoke_status type boolean

--- lib/models/patient_smoking_habit.py
+++ lib/models/patient_smoking_habit.py
@@ -1,7 +1,9 @@
 import uuid
-from sqlalchemy import UUID, Column, ForeignKey, Integer, String
-from lib.models import Base
+
+from sqlalchemy import UUID, Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
+
+from lib.models import Base
 
 
 class PatientSmokingHabit(Base):
@@ -11,7 +13,7 @@ class PatientSmokingHabit(Base):
         UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True
     )
     patient_id = Column(UUID(as_uuid=True), ForeignKey("patients.patient_id"))
-    smoke_status = Column(String(20))
+    smoke_status = Column(Boolean)
     years_of_smoking = Column(Integer, nullable=True)
     cigarettes_per_day = Column(Integer, nullable=True)
     quit_years_ago = Column(Integer, nullable=True)
