ef9909fdba9e904097f737ee17d8590bf5f7b663
Mukhtar Ahmed <mukhtar@ahealth.in>
2024-11-07 19:43:57 +0530

    Update patient_sleep_habit.py

--- lib/models/patient_sleep_habit.py
+++ lib/models/patient_sleep_habit.py
@@ -1,7 +1,9 @@
 import uuid
+
 from sqlalchemy import UUID, Boolean, Column, Float, ForeignKey, String, Time
-from lib.models import Base
 from sqlalchemy.orm import relationship
+
+from lib.models import Base
 
 
 class PatientSleepHabit(Base):
@@ -10,9 +12,9 @@ class PatientSleepHabit(Base):
     patient_id = Column(
         UUID(as_uuid=True), ForeignKey("patients.patient_id"), primary_key=True
     )
-    sleep_quality = Column(String(50))
-    wake_up_fresh = Column(Boolean)
-    drowsy_day = Column(Boolean)
+    sleep_quality = Column(String(50), nullable=True)
+    wake_up_fresh = Column(Boolean, nullable=True)
+    drowsy_day = Column(Boolean, nullable=True)
     average_sleep_duration = Column(Float, nullable=True)
     wake_up_time = Column(Time, nullable=True)
     bed_time = Column(Time, nullable=True)
