f4d14d560b08c059146e56ecdeb9541811225c3d
Mukhtar Ahmed <mukhtar@ahealth.in>
2024-11-07 21:40:37 +0530

    Update delete.py

--- rest_server/care_provider/profile/delete.py
+++ rest_server/care_provider/profile/delete.py
@@ -19,7 +19,7 @@ from rest_server.response_models import ErrorResponse, SuccessResponse
 from .router import router
 
 
-@router.delete("/{care_provider_id}", response_model=SuccessResponse)
+@router.delete("", response_model=SuccessResponse)
 async def delete_care_provider_profile(
     request: Request,
     care_provider_id: str,
