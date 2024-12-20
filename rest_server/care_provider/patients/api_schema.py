06003840245b4d5ba7903d4c2686c99e82ea0689
Mukhtar Ahmed <mukhtar@ahealth.in>
2024-12-21 03:50:21 +0530

    --

--- rest_server/care_provider/patients/api_schema.py
+++ rest_server/care_provider/patients/api_schema.py
@@ -1,11 +1,15 @@
-from typing import List
+from typing import List, Optional
 
 from pydantic import BaseModel
 
 from lib.schemas.care_provider import CareProvider
+from lib.schemas.health_facility import HealthFacility
+from lib.schemas.package import Package
 from lib.schemas.patient import Patient
 from rest_server.response_models import SuccessResponse
 
 
 class CareProviderPatients(Patient):
     care_providers: List[CareProvider] = []
+    package: Optional[Package] = None
+    health_facility: Optional[HealthFacility] = None
