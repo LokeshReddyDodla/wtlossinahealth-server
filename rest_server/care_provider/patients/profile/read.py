28165e0f49bfdacc815b7309a5fa732502157812
Mukhtar Ahmed <mukhtar@ahealth.in>
2025-02-11 16:47:58 +0530

    --

--- rest_server/care_provider/patients/profile/read.py
+++ rest_server/care_provider/patients/profile/read.py
@@ -2,10 +2,12 @@ from fastapi import Depends, HTTPException, status
 
 from lib.dependencies.auth.care_provider_auth import get_current_care_provider
 from lib.dependencies.service_dependencies import \
-    get_care_provider_profile_service
+    get_care_provider_profile_service,get_cgm_report_service
+    
 from lib.models.care_provider import CareProvider as CareProviderModel
 from lib.services.care_provider_profile_service import \
     CareProviderProfileService
+from lib.services.cgm_report_service import CGMReportService
 from lib.utils.care_provider_permissions import (CareProviderFeature,
                                                  CareProviderPermissionAction)
 from lib.utils.http_exceptions import raise_http_exception
@@ -21,6 +23,8 @@ async def get_patients(
     care_provider_profile_service: CareProviderProfileService = Depends(
         get_care_provider_profile_service
     ),
+    cgm_report_service: CGMReportService = Depends(get_cgm_report_service),
+
     current_care_provider: CareProviderModel = Depends(
         get_current_care_provider(
             CareProviderPermissionAction.READ, CareProviderFeature.PATIENTS
@@ -33,12 +37,22 @@ async def get_patients(
                 str(current_care_provider.care_provider_id)
             )
         )
-
+        
+        updated_patients = []
+        
+        for patient in patients:
+            cgm_reports = await cgm_report_service.fetch_reports(
+                str(patient.patient_id)
+            )
+            updated_patient = {
+                **CareProviderPatients.from_orm(patient).model_dump(),
+                "cgm_reports": cgm_reports,
+            }
+            updated_patients.append(updated_patient)
+            
         return SuccessResponse(
             message="Patients fetched successfully",
-            data=[
-                CareProviderPatients.from_orm(patient) for patient in patients
-            ],
+            data=updated_patients,
         )
     except HTTPException as e:
         raise e
