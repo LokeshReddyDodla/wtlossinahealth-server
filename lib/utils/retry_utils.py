073307166cd7253ade4668162c487e053d4281f4
Mukhtar Ahmed <mukhtar@ahealth.in>
2025-03-27 15:54:05 +0530

    Update retry_utils.py

--- lib/utils/retry_utils.py
+++ lib/utils/retry_utils.py
@@ -1,14 +1,18 @@
 import time
 
-def retry_request(func, max_retries=3, delay=2, *args, **kwargs):
-    attempts = 0
-    while attempts < max_retries:
-        print("==> Retrying request count: %d" % attempts)
-        try:
-            return func(*args, **kwargs)
-        except Exception as e:
-            print(f"Attempt {attempts + 1} failed: {e}")
-            attempts += 1
-            if attempts < max_retries:
-                time.sleep(delay)
-    raise Exception(f"Failed after {max_retries} attempts")
+from tenacity import (retry, retry_if_exception_type, stop_after_attempt,
+                      wait_fixed)
+
+
+def retry_request(func, *args, max_retries=3, delay=2, **kwargs):
+    @retry(
+        stop=stop_after_attempt(max_retries),
+        wait=wait_fixed(delay),
+        retry=retry_if_exception_type(Exception),  # retries on any exception
+        reraise=True,  # raises the last exception after all retries
+    )
+    def wrapped_func():
+        print("==> Retrying request")
+        return func(*args, **kwargs)
+
+    return wrapped_func()
