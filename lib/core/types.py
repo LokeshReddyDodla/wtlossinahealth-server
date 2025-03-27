303f60388a66e8ebdcc907631d4f2404c2609564
Mukhtar Ahmed <mukhtar@ahealth.in>
2025-03-27 17:15:21 +0530

    Update types.py

--- lib/core/types.py
+++ lib/core/types.py
@@ -38,3 +38,8 @@ AiConversationTypeLiteral = Literal[
 OpenAIModelLiteral = Literal[
     "gpt-4o", "gpt-4o-mini", "gpt-4-mini", "gpt-3.5-turbo", "davinci"
 ]
+
+GeminiAIModelLiteral = Literal[
+    "gemini-1.5-flash",
+    "gemini-2.0-flash",
+]
