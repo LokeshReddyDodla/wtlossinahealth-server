8cee7ba60876ad0fbb194a9f737195c313ab023e
Mukhtar Ahmed <mukhtar@ahealth.in>
2025-04-30 17:55:52 +0530

    Update update.py

--- rest_server/care_provider/profile/update.py
+++ rest_server/care_provider/profile/update.py
@@ -1,4 +1,4 @@
-from fastapi import Body, Depends, HTTPException, Request, status
+from fastapi import Depends, HTTPException, Request, status
 from sqlalchemy.exc import IntegrityError
 
 from lib.dependencies.auth.care_provider_auth import get_current_care_provider
@@ -12,6 +12,7 @@ from lib.utils.care_provider_permissions import (
     CareProviderPermissionAction,
 )
 from lib.utils.http_exceptions import raise_http_exception
+from rest_server.care_provider.profile.api_schema import SetPasswordRequest
 from rest_server.response_models import SuccessResponse
 
 from .router import router
@@ -54,7 +55,7 @@ async def update_care_provider_profile(
 
 @router.put("/set-password", response_model=SuccessResponse)
 async def set_care_provider_password(
-    raw_password: str = Body(...),
+    request_data: SetPasswordRequest,
     care_provider_profile_service: CareProviderProfileService = Depends(
         get_care_provider_profile_service
     ),
@@ -68,7 +69,7 @@ async def set_care_provider_password(
     try:
         await care_provider_profile_service.set_care_provider_password(
             care_provider_id=str(current_care_provider.care_provider_id),
-            raw_password=raw_password,
+            raw_password=request_data.raw_password,
         )
 
         return SuccessResponse(message="Password set successfully.")
