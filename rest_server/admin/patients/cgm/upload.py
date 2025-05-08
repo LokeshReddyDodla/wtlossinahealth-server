45ac75d3714d2d1c4a418210ac3ef024afee0f8b
Mukhtar Ahmed <mukhtar@ahealth.in>
2025-05-08 23:35:08 +0530

    -

--- rest_server/admin/patients/cgm/upload.py
+++ rest_server/admin/patients/cgm/upload.py
@@ -1,29 +1,19 @@
-from datetime import datetime
-from io import StringIO
-from typing import Union
-
-import pandas as pd
-from fastapi import (APIRouter, Depends, File, HTTPException, Request, status,
-                     UploadFile)
-from lib.dependencies.service_dependencies import get_cgm_service
-from lib.services.cgm_upload_service import CGMUploadService
-from lib.utils.http_exceptions import raise_http_exception
+from fastapi import APIRouter, Depends, File, Request, UploadFile, status
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.future import select
-from sqlalchemy.orm import Session, selectinload
 
 from lib.dependencies.auth.admin_auth import get_current_admin
 from lib.dependencies.database import get_postgres_session
+from lib.dependencies.service_dependencies import get_cgm_service
 from lib.models.admin import Admin
-from lib.models.patient import Patient
-from lib.models.patient_connected_app import PatientConnectedApp
-from rest_server.response_models import ErrorResponse, SuccessResponse
+from lib.services.cgm_upload_service import CGMUploadService
+from lib.utils.http_exceptions import raise_http_exception
+from rest_server.response_models import SuccessResponse
 
 router = APIRouter(prefix="/admin/patient")
 
 
-@router.post("/cgm/upload", tags=["Admin Patient"])
-async def admin_upload_cgm_data(
+@router.post("/upload/libreview-raw-csv", tags=["Admin Patient"])
+async def admin_upload_libreview_raw_csv(
     request: Request,
     patient_id: str,
     file: UploadFile = File(...),
@@ -36,10 +26,8 @@ async def admin_upload_cgm_data(
             patient_id=str(patient_id),
             file_contents=await file.read(),
         )
-       
-        return SuccessResponse(
-            message="CGM data uploaded and stored successfully."
-        )
+
+        return SuccessResponse(message="CGM data uploaded and stored successfully.")
 
     except Exception as e:
         raise_http_exception(
