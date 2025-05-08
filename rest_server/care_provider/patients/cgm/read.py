8de9e81f29a3e4498116b7b5836245e96a6c7c2b
Mukhtar Ahmed <mukhtar@ahealth.in>
2025-05-08 21:17:08 +0530

    Update read.py

--- rest_server/care_provider/patients/cgm/read.py
+++ rest_server/care_provider/patients/cgm/read.py
@@ -2,8 +2,14 @@ from datetime import date
 
 from fastapi import Depends, HTTPException, Query, Request, status
 
+from lib.dependencies.auth.care_provider_auth import get_current_care_provider
 from lib.dependencies.service_dependencies import get_cgm_report_service
+from lib.models.care_provider import CareProvider as CareProviderModel
 from lib.services.cgm_report_service import CGMReportService
+from lib.utils.care_provider_permissions import (
+    CareProviderFeature,
+    CareProviderPermissionAction,
+)
 from lib.utils.http_exceptions import raise_http_exception
 from rest_server.response_models import SuccessResponse
 
@@ -16,11 +22,11 @@ async def get_cgm_day_report(
     patient_id: str = Query(...),
     date: date = Query(...),
     cgm_report_service: CGMReportService = Depends(get_cgm_report_service),
-    # current_care_provider: CareProviderModel = Depends(
-    #     get_current_care_provider(
-    #         CareProviderPermissionAction.READ, CareProviderFeature.REPORTS
-    #     )
-    # ),
+    current_care_provider: CareProviderModel = Depends(
+        get_current_care_provider(
+            CareProviderPermissionAction.READ, CareProviderFeature.REPORTS
+        )
+    ),
 ):
     try:
         day_report = await cgm_report_service.fetch_day_report(patient_id, date)
