14e3d6a44d175f171704942eafb82b1deab10e77
Mukhtar Ahmed <mukhtar@ahealth.in>
2025-05-09 20:17:14 +0530

    Update router.py

--- rest_server/admin/patients/connected_apps/router.py
+++ rest_server/admin/patients/connected_apps/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-router = APIRouter(prefix="/connected-apps", tags=["Admin - Connected Apps"])
+router = APIRouter(prefix="/patients/connected-apps", tags=["Admin - Patients Connected Apps"])
 
 from .read import *
 
