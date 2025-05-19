c177c26737e5bfcc07500ff05fd2aa33a19fd15e
Mukhtar Ahmed <mukhtar@ahealth.in>
2025-05-19 22:14:48 +0530

    -

---
+++ rest_server/care_provider/patients/profile/create.py
@@ -0,0 +1,57 @@
+from fastapi import Depends, HTTPException, Request, status
+from sqlalchemy import or_
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+
+from lib.dependencies.auth.care_provider_auth import get_current_care_provider
+from lib.dependencies.database import get_postgres_session
+from lib.dependencies.service_dependencies import get_patient_profile_service
+from lib.models.patient import Patient
+from lib.schemas.patient import Patient as PatientSchema
+from lib.schemas.patient import PatientCreate
+from lib.services.patient_profile_service import PatientProfileService
+from lib.utils.care_provider_permissions import (
+    CareProviderFeature,
+    CareProviderPermissionAction,
+)
+from lib.utils.http_exceptions import raise_http_exception
+from rest_server.response_models import SuccessResponse
+from lib.models.care_provider import CareProvider as CareProviderModel
+
+from .router import router
+
+
+@router.post(path="/profile", response_model=SuccessResponse)
+async def create_patient(
+    request: Request,
+    patient_data: PatientCreate,
+    patient_profile_service: PatientProfileService = Depends(
+        get_patient_profile_service
+    ),
+    current_care_provider: CareProviderModel = Depends(
+        get_current_care_provider(
+            CareProviderPermissionAction.CREATE, CareProviderFeature.PATIENTS
+        )
+    ),
+):
+    try:
+        new_patient = await patient_profile_service.create_patient(
+            patient_data=patient_data,
+            creating_care_provider=current_care_provider,
+        )  # type: ignore
+        result = PatientSchema.from_orm(new_patient)
+
+        return SuccessResponse(
+            message="Patient created and assigned to care provider",
+            data=result,
+        )
+
+    except HTTPException as http_exc:
+        raise http_exc
+    except Exception as e:
+        raise_http_exception(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            message="Internal Server Error",
+            detail=str(e),
+        )
--- rest_server/patients/profile/create.py
+++ rest_server/patients/profile/create.py
@@ -1,4 +1,3 @@
-
 from fastapi import Depends, HTTPException, Request, status
 from sqlalchemy import or_
 from sqlalchemy.exc import IntegrityError
@@ -13,57 +12,3 @@ from lib.utils.http_exceptions import raise_http_exception
 from rest_server.response_models import SuccessResponse
 
 from .router import router
-
-
-@router.post(path="/basic", response_model=SuccessResponse)
-async def create_basic_patient(
-    request: Request,
-    patient_data: PatientCreate,
-    session: AsyncSession = Depends(get_postgres_session),
-):
-    try:
-        # Check if the patient already exists
-        existing_patient = await session.execute(
-            select(Patient).filter(
-                or_(
-                    Patient.email == patient_data.email,
-                    Patient.phone_number == patient_data.phone_number,
-                )
-            )
-        )
-        existing_patient = existing_patient.scalar_one_or_none()
-
-        if existing_patient:
-            raise_http_exception(
-                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                message="Patient already exists",
-            )
-
-        # Create a new patient
-        new_patient = Patient(**patient_data.dict())
-        session.add(new_patient)
-        await session.commit()
-        await session.refresh(new_patient)
-
-        result = PatientSchema.from_orm(new_patient)
-
-        return SuccessResponse(
-            message="Patient basic data created successfully.",
-            data=result,
-        )
-    except HTTPException as http_exc:
-        raise http_exc
-    except IntegrityError as e:
-        await session.rollback()
-        raise_http_exception(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            message="Integrity Error",
-            detail=str(e),
-        )
-    except Exception as e:
-        await session.rollback()
-        raise_http_exception(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            message="Internal Server Error",
-            detail=str(e),
-        )
