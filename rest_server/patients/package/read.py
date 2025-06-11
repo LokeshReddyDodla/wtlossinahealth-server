b98d4d7b74ad0f124552c2d97e631099bebc4e49
Mukhtar Ahmed <mukhtar@ahealth.in>
2025-06-11 12:12:50 +0530

    add get_package_by_code_api

--- rest_server/patients/package/read.py
+++ rest_server/patients/package/read.py
@@ -4,19 +4,21 @@ from fastapi import Depends, HTTPException, Request, status
 
 from lib.dependencies.auth.patient_auth import get_current_patient
 from lib.dependencies.service_dependencies import (
+    get_package_service,
     get_patient_package_assignment_service,
     get_patient_profile_service,
 )
 from lib.models.patient import Patient
-from lib.schemas.package import Package
 from lib.schemas.patient_package_assignment import (
     PatientPackageAssignmentWithDetail,
 )
+from lib.services.package_service import PackageService
 from lib.services.patient_package_assignment_service import (
     PatientPackageAssignmentService,
 )
 from lib.services.patient_profile_service import PatientProfileService
 from lib.utils.http_exceptions import raise_http_exception
+from rest_server.patients.package.api_schema import PackageByCodeResponse
 from rest_server.response_models import SuccessResponse
 
 from .router import router
@@ -42,6 +44,34 @@ async def get_patient_packages_api(
                 PatientPackageAssignmentWithDetail.from_orm(a)
                 for a in assignments
             ],
+        )
+
+    except HTTPException as http_exc:
+        raise http_exc
+    except Exception as e:
+        raise_http_exception(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            message="Internal Server Error",
+            detail=str(e),
+        )
+
+
+@router.get(path="/code", response_model=SuccessResponse)
+async def get_package_by_code_api(
+    request: Request,
+    code: str,
+    package_service: PackageService = Depends(get_package_service),
+    current_patient: Patient = Depends(get_current_patient),
+):
+    try:
+        package = await package_service.fetch_package_by_code(
+            package_code=code, detailed=True
+        )
+
+      
+         return SuccessResponse(
+            message="Package retrieved successfully",
+            data=PackageByCodeResponse.from_orm(package)
         )
 
     except HTTPException as http_exc:
