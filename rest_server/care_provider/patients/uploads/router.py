817dd9aa195e7ab5ca80380db95e2db5cb543e2b
Mukhtar Ahmed <mukhtar@ahealth.in>
2025-08-21 14:17:03 +0530

    Update router.py

--- rest_server/care_provider/patients/uploads/router.py
+++ rest_server/care_provider/patients/uploads/router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 router = APIRouter(
     prefix="/patients/{patient_id}/uploads",
-    tags=["Care Provider - Patients Uploads"],
+    tags=["Care Provider - Patient Uploads"],
 )
 
 from .create import *
