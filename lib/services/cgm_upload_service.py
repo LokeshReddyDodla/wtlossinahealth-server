758562bd2fbcb3e167499ac123d494007d9f1c3b
Mukhtar Ahmed <mukhtar@ahealth.in>
2025-09-13 13:01:28 +0530

    update libreview sensor report generator logic

--- lib/services/cgm_upload_service.py
+++ lib/services/cgm_upload_service.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from io import StringIO
-from typing import List
+from typing import List, Tuple
 
 import pandas as pd
 from lib.core.postgres_store import PostgresStore
@@ -13,6 +13,9 @@ from lib.utils.http_exceptions import raise_http_exception
 from sqlalchemy.future import select
 from sqlalchemy.orm import selectinload
 
+from lib.utils.libre_view_sensor_report_generator import (
+    SensorLifecycleReportGenerator,
+)
 from lib.utils.postgres_session_decorator import with_postgres_session
 
 
@@ -79,23 +82,45 @@ class CGMUploadService:
             cgm_report_periods = cgm_data_utils.generate_all_report_periods(df)
             print("==> cgm_report_periods: ", cgm_report_periods)
 
+            gen = SensorLifecycleReportGenerator(df)
+            reports = gen.generate_reports()
+
+            report_periods: List[Tuple[str, str]] = [
+                (
+                    r["start"].strftime("%Y-%m-%d %H:%M:%S"),
+                    r["end"].strftime("%Y-%m-%d %H:%M:%S"),
+                )
+                for r in reports
+            ]
+            print("==> report_periods: ", report_periods)
+            for r in reports:
+                print(
+                    r["start"],
+                    "→",
+                    r["end"],
+                    "days:",
+                    r["duration_h"] / 24,
+                    "coverage:",
+                    round(r["coverage"], 2),
+                )
+
             # Write data to ClickHouse
-            self.clickhouse_store.write_data("aihealth.cgm_data", data_points)
+            # self.clickhouse_store.write_data("aihealth.cgm_data", data_points)
 
             # Update last_sync_timestamp for the connected app if it exists
-            connected_app_result = await postgres_session.execute(
-                select(PatientConnectedApp)
-                .where(PatientConnectedApp.patient_id == patient_id)
-                .options(selectinload(PatientConnectedApp.libreview))
-            )
-            connected_app = connected_app_result.scalars().first()
-            if connected_app and connected_app.libreview:
-                connected_app.libreview.last_sync_timestamp = datetime.now()
-                await postgres_session.commit()
+            # connected_app_result = await postgres_session.execute(
+            #     select(PatientConnectedApp)
+            #     .where(PatientConnectedApp.patient_id == patient_id)
+            #     .options(selectinload(PatientConnectedApp.libreview))
+            # )
+            # connected_app = connected_app_result.scalars().first()
+            # if connected_app and connected_app.libreview:
+            #     connected_app.libreview.last_sync_timestamp = datetime.now()
+            #     await postgres_session.commit()
 
-            generate_cgm_reports_for_patient.delay(
-                patient_id, cgm_report_periods
-            )
+            # generate_cgm_reports_for_patient.delay(
+            #     patient_id, cgm_report_periods
+            # )
 
         except Exception as e:
             raise_http_exception(
