72c9908fd808743fffbe0cdc3f2fcbd127f4a676
Mukhtar Ahmed <itssmukhtar@gmail.com>
2025-12-31 22:29:42 +0530

    create v1 -> list health facilities

--- rest_server/v1/router.py
+++ rest_server/v1/router.py
@@ -5,8 +5,10 @@ router = APIRouter(prefix="/v1", tags=["V1"])
 from .packages.router import router as packages_router
 from .care_providers.router import router as care_providers_router
 from .patients.router import router as patients_router
+from .health_facilities.router import router as health_facilities_router
 
 router.include_router(packages_router)
 router.include_router(care_providers_router)
 router.include_router(patients_router)
+router.include_router(health_facilities_router)
 
